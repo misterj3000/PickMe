62675ebc52924046e58d3f489f59f9a96d303faf
misterj3000 <misterj3000@users.noreply.github.com>
2018-02-14 10:27:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+This is a JavaScript (JS) file.
+JavaScript is the programming language that powers the web.
+
+To use this file, place the following <script> tag just before the closing </body> tag in your HTML file, making sure that the filename after "src" matches the name of your file...
+
+    <script src="script.js"></script>
+
+Learn more about JavaScript at https://developer.mozilla.org/en-US/Learn/JavaScript
+
+When you're done, you can delete all of this grey text, it's just a comment.
+
+
+function greetMe(name) {
+  var today = new Date().toDateString();
+  console.log("Hello, today is " + today);
+}
+
+greetMe("World");
+
+*/
+
+let studentsNames = []
+
+//Array of Student identifiers
+var myList = ['Student 1', 'Student 2', 'Student 3','Student 4','Student 5','Student 6','Student 7' ,'Student 8' , 'Student 9','Student 10', 'Student 11', 'Student 12', 'Student 13','Student 14','Student 15','Student 16','Student 17' ,'Student 18',  'Student 19','Student 20','Student 21','Student 22','Student 23','Student 24','Student 25','Student 26','Student 27','Student 28','Student 29','Student 30'];
+
+//  List Randomizer Formula
+var randList = Math.floor(Math.random() * myList.length); 
+
+
+
+
